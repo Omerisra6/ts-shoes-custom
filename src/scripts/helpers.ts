2331a510a23750cf802de7d6012d3003db707439
Omerisra06 <Omerisraeli6@gmail.com>
2023-04-22 21:57:52 +0300

    Tweak: Cookie functions transferred to a different file

--- src/scripts/helpers.ts
+++ src/scripts/helpers.ts
@@ -1,5 +1,3 @@
-import { Product } from "../products.js"
-
 export function _( selector: any ): any
 {
     return document.querySelector( selector )
@@ -27,72 +25,6 @@ export const debounce = (fn: Function, ms = 300) => {
     };
 };
 
-export function setCookie( cname: string, cvalue: any, exdays: number ) 
-{    
-    const d = new Date();
-    d.setTime( d.getTime() + ( exdays*24*60*60*1000 ) );
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-export function getCookie( cname: string ) :string
-{
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent( document.cookie );
-    let ca = decodedCookie.split( ';' );
-    for( let i = 0; i <ca.length; i++ ) 
-    {
-        let c = ca[ i ];
-        while ( c.charAt(0) == ' ' ) 
-        {
-            c = c.substring( 1) ;
-        }
-
-        if ( c.indexOf(name) == 0 ) 
-        {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-export function addProductToCookieArray( cname: string, newProduct: Product )
-{
-    const productsCookie: string = getCookie( cname )
-
-    const productsArray: Array< Product > = ! productsCookie ? [] : JSON.parse( productsCookie ) 
-    productsArray.push( newProduct )
-        
-    setCookie( cname, JSON.stringify( productsArray ), 1 )
-}
-
-export function removeProductFromCookieArray( cname: string, producyIndex: number )
-{
-    const productsCookie: string = getCookie( cname )
-    const productsArray: Array< Product > = JSON.parse( productsCookie ) 
-
-    productsArray.splice( producyIndex, 1 )
-        
-    setCookie( cname, JSON.stringify( productsArray ), 1 )
-}
-
-export function findProductIndexInCookieArray( cname: string, searchedProduct: Product): number
-{
-    const productsCookie: string = getCookie( cname )
-
-    if ( ! productsCookie ) 
-    {
-        return -1
-    }   
-
-    const productsArray: Array< Product > = JSON.parse( productsCookie ) 
-    
-    return productsArray.findIndex( ( product: Product ) => {
-        
-        return product.id === searchedProduct.id
-    })
-}
-
 export function selectFirstInput( inputsContainer: Element )
 {
     const colorInputs: NodeListOf<Element>  = inputsContainer.querySelectorAll( 'input[name="color"]' )    
@@ -100,3 +32,22 @@ export function selectFirstInput( inputsContainer: Element )
     
     firstColorInput.checked = true    
 }
+
+export function adjustQuantityValue( inputElement: HTMLInputElement, adjustment: number )
+{
+    inputElement.value += adjustment
+}
+
+export function countObjectsWithAttribute(arr: Array< { [ key: string] : any } >, key: string, value: any) 
+{
+    return arr.reduce( ( count, obj ) => {
+
+      if ( obj.hasOwnProperty( key ) && obj[ key ] === value ) 
+      {
+        return count + 1;
+      }
+      
+      return count;
+    }, 0);
+  }
+  
