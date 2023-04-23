fc6bc039d96c6147d1c724be02ddabc07a362d00
Omerisra06 <Omerisraeli6@gmail.com>
2023-04-23 19:01:54 +0300

    New: Cart product type added and product type updated

--- src/products.ts
+++ src/products.ts
@@ -1,13 +1,21 @@
-export type Product = {
+export interface Product
+{
     id: string,
     company: string,
     name: string,
     svgFileName: string,
-    customization?: string
+    price: number,
+    compareAtPrice?: number,
+}
+
+export interface CartProduct extends Product
+{
+    customization: string,
+    quantity?: number
 }
 
 export const productList: Array< Product > = 
 [
-    { id: "1", company: "nike", name: "AIR JORDAN ONE", svgFileName: "air-jordan-1.svg" },
-    { id: "2", company: "nike", name: "AIR JORDAN FOUR", svgFileName: "air-jordan-four.svg" }
+    { id: "1", company: "nike", name: "AIR JORDAN ONE", svgFileName: "air-jordan-1.svg", price: 120, compareAtPrice: 159 },
+    { id: "2", company: "nike", name: "AIR JORDAN FOUR", svgFileName: "air-jordan-four.svg", price: 120, compareAtPrice: 159 }
 ]
